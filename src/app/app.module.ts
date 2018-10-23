059318dce2ba8597f379c837db62ac51666ab640
Krishnamoorthy M <krishnamoorthym@SRISERVER2012.COM>
2018-10-23 15:58:05 +0530

    Child component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrainComponent } from './brain/brain.component';
+import { HeartComponent } from './heart/heart.component';
+import { FootComponent } from './foot/foot.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BrainComponent,
+    HeartComponent,
+    FootComponent
   ],
   imports: [
     BrowserModule,
