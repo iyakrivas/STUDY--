1f90efd5b93fc8fc55eb456dbd9f7448203550af
Krishnamoorthy M <krishnamoorthym@SRISERVER2012.COM>
2018-10-23 16:12:56 +0530

    Child-component deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrainComponent } from './brain/brain.component';
-import { HeartComponent } from './heart/heart.component';
-import { FootComponent } from './foot/foot.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    BrainComponent,
-    HeartComponent,
-    FootComponent
+    AppComponent
+    
   ],
   imports: [
     BrowserModule,
